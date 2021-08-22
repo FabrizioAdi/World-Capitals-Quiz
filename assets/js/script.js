f15b638b70c1daa2bdbcf21b87d722cbe78f697c
FabrizioAdi <adriannoszczyk@gmail.com>
2021-08-22 15:02:19 +0000

    Add a function and first question with the answer to javascript file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,43 @@
 // select all elements
 let start = document.getElementById('start');
 let questionBox = document.getElementById('question-box');
+
+let randomQuestion, currentQuestion
+
 start.addEventListener('click', startGame);
 
 function startGame() {
     console.log('start')
     start.classList.add('hide')
+    randomQuestion = questions.sort(()=> Math.random() - .5)
+    currentQuestion = 0
     questionBox.classList.remove('hide')
+    nextQuestion()
+
+}
+
+function nextQuestion {
+    showQuestion(randomQuestion[currentQuestion])
+}
+
+function showQuestion(question)
+
+function answerChoice {
 
 }
 
 
 
+// create questions
+let question = [
+    { 
+        question: 'What city is the capital of Brazil',
+        answers: [
+             { text: 'Buenos Aires', correct: false }
+             { text: 'Paris', correct: false }
+             { text: 'Brasilia', correct: true }
+             { text: 'Tokyo', correct: false } 
+        ]
+    }    
+];
+    
