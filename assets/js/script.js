9d2bc60083969a63a77ef0021f9e57ef421a3521
FabrizioAdi <adriannoszczyk@gmail.com>
2021-08-23 12:16:25 +0000

    Bug undefined fix in the answers section showing capitals

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,12 +31,12 @@ function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answers => {
         let button = document.createElement('button')
-        button.innerText = answers.text
+        button.innerText = answers.choice
         button.classList.add('button')
         if (answers.correct) {
             button.dataset.correct = answers.correct
         }
-        button.addEventListener('click', answerChoice)
+        button.addEventListener('click', answersChoice)
         answersElement.appendChild(button)
     });
 
@@ -48,7 +48,7 @@ function resetState() {
 
 }
 
-function answerChoice(e) {
+function answersChoice(e) {
 let selectedButton = e.target
 let correct = selectedButton.dataset.correct
 statusClass(document.body, correct)
@@ -78,100 +78,100 @@ let questions = [
     { 
         question: 'Which city is the capital of Brazil ?',
         answers: [
-             { choice1: 'Buenos Aires', correct: false },
-             { choice2: 'Paris', correct: false },
-             { choice3: 'Brasilia', correct: true },
-             { choice4: 'Tokyo', correct: false }
+             { choice: 'Buenos Aires', correct: false },
+             { choice: 'Paris', correct: false },
+             { choice: 'Brasilia', correct: true },
+             { choice: 'Tokyo', correct: false }
         ]
     },
 //Q2
     {
         question: 'Which city is the capital of Canada ?',
         answers: [
-          { choice1: 'Dublin', correct: false },
-          { choice2: 'Montevideo', correct: false },
-          { choice3: 'Panama City', correct: false },
-          { choice4: 'Ottawa', correct: true }
+          { choice: 'Dublin', correct: false },
+          { choice: 'Montevideo', correct: false },
+          { choice: 'Panama City', correct: false },
+          { choice: 'Ottawa', correct: true }
         ]
       },
 //Q3
 {
     question: 'Which city is the capital of Portugal ?',
     answers: [
-      { choice1: 'Lisbon', correct: true },
-      { choice2: 'Warsaw', correct: false },
-      { choice3: 'Bern', correct: false },
-      { choice4: 'Accra', correct: false }
+      { choice: 'Lisbon', correct: true },
+      { choice: 'Warsaw', correct: false },
+      { choice: 'Bern', correct: false },
+      { choice: 'Accra', correct: false }
     ]
   },
 //Q4
 {
 question: 'Which city is the capital of Australia ?',
     answers: [
-      { choice1: 'Canberra', correct: true },
-      { choice2: 'Madrid', correct: false },
-      { choice3: 'Auckland', correct: false },
-      { choice4: 'Kyiv', correct: false }
+      { choice: 'Canberra', correct: true },
+      { choice: 'Madrid', correct: false },
+      { choice: 'Auckland', correct: false },
+      { choice: 'Kyiv', correct: false }
     ]
   },
 //Q5
 {
 question: 'Which city is the capital of France ?',
     answers: [
-      { choice1: 'London', correct: false },
-      { choice2: 'Rome', correct: false },
-      { choice3: 'Santiago', correct: false },
-      { choice4: 'Paris', correct: true }
+      { choice: 'London', correct: false },
+      { choice: 'Rome', correct: false },
+      { choice: 'Santiago', correct: false },
+      { choice: 'Paris', correct: true }
     ]
   },
 //Q6
 {
 question: 'Which city is the capital of England?',
     answers: [
-      { choice1: 'Prague', correct: false },
-      { choice2: 'London', correct: true },
-      { choice3: 'Mexico City', correct: false },
-      { choice4: 'Abuja', correct: false }
+      { choice: 'Prague', correct: false },
+      { choice: 'London', correct: true },
+      { choice: 'Mexico City', correct: false },
+      { choice: 'Abuja', correct: false }
     ]
   },
 //Q7
 {
 question: 'Which city is the capital of South Korea ?',
     answers: [
-      { choice1: 'Seoul', correct: true },
-      { choice2: 'Hanoi', correct: false },
-      { choice3: 'Buenos Aires', correct: false },
-      { choice4: 'Tokyo', correct: false }
+      { choice: 'Seoul', correct: true },
+      { choice: 'Hanoi', correct: false },
+      { choice: 'Buenos Aires', correct: false },
+      { choice: 'Tokyo', correct: false }
     ]
   },
 //Q8
 {
 question: 'Which city is the capital of USA ?',
     answers: [
-      { choice1: 'Athens', correct: false },
-      { choice2: 'Cairo', correct: false },
-      { choice3: 'Washington', correct: true },
-      { choice4: 'Berlin', correct: false }
+      { choice: 'Athens', correct: false },
+      { choice: 'Cairo', correct: false },
+      { choice: 'Washington', correct: true },
+      { choice: 'Berlin', correct: false }
     ]
   },
 //Q9
 {
 question: 'Which city is the capital of Germany?',
     answers: [
-      { choice1: 'Vienna', correct: false },
-      { choice2: 'Moscow', correct: false },
-      { choice3: 'London', correct: false },
-      { choice4: 'Berlin', correct: true }
+      { choice: 'Vienna', correct: false },
+      { choice: 'Moscow', correct: false },
+      { choice: 'London', correct: false },
+      { choice: 'Berlin', correct: true }
     ]
   },
 //Q10
 {
 question: 'Which city is the capital of Russia?',
     answers: [
-      { choice1: 'Moscow', correct: true },
-      { choice2: 'Vilnius', correct: false },
-      { choice3: 'Phnom Penh', correct: false },
-      { choice4: 'Washington', correct: false }
+      { choice: 'Moscow', correct: true },
+      { choice: 'Vilnius', correct: false },
+      { choice: 'Phnom Penh', correct: false },
+      { choice: 'Washington', correct: false }
     ]
   },
 
